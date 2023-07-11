feef933cdb9a10ea3ec335f51dc6ab8fef8af921
ganukov <ganukoff@gmail.com>
2023-07-11 14:48:32 +0100

    N number vector generator solution

---
+++ n_number_verctor.py
@@ -0,0 +1,13 @@
+def gen01(idx, vector):
+    if idx >= len(vector):
+        print(*vector, sep=' ')
+        return
+        # range(2) because we need 0 and 1
+    for num in range(1, len(vector) + 1):
+        vector[idx] = num
+        gen01(idx + 1, vector)
+
+
+n = int(input())
+vector = [0] * n
+gen01(0, vector)
